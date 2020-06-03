d533663ae9ecc5b3a19663f6855e9f62233616d7
haw0k <cyril.zlachevsky@gmail.com>
2020-06-03 21:02:43 +0300

    end lesson 1

--- src/Counter.js
+++ src/Counter.js
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 import { observable } from 'mobx';
 import { observer} from 'mobx-react';
 
+
 @observer class Counter extends Component {
   @observable count = 0;
 
   render() {
     return(
       <div>
-        Counter: {this.count} <br />
-        <button onClick={this.handleDec}>-</button>
-        <button onClick={this.handleInc}>+</button>
+        Counter: {this.props.store.count} <br />
+        <button onClick={this.handleInc}>-</button>
+        <button onClick={this.handleDec}>+</button>
       </div>
     )
   }
 
   handleDec = () => {
-
+    this.props.store.increment()
   }
 
   handleInc = () => {
-
+    this.props.store.decrement()
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
+import { observable } from 'mobx';
+
+const appState = observable({
+  count : 0
+})
+
+appState.increment = function() {
+  this.count++
+}
+
+appState.decrement = function() {
+  this.count--
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counter store={appState} />
   </React.StrictMode>,
   document.getElementById('root')
 );
